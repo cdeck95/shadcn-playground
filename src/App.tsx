091c61a9a0653da37bb842d4218436235073b3ee
Chris Deck <deckchris95@gmail.com>
2024-02-08 22:51:00 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./globals.css";
+import { CardWithForm } from "./test-components/CardWithForm";
+import { Checkbox } from "./@/components/ui/checkbox";
+import { CardWithNotifications } from "./test-components/CardWithNotifications";
+import { CheckboxDemo } from "./test-components/CheckBoxDemo";
+import { AccordionHeader } from "@radix-ui/react-accordion";
+import { Accordion } from "@/components/ui/accordion";
+import { AccordionDemo } from "./test-components/Accordion";
+import { DatePicker } from "./test-components/DatePicker";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CheckboxDemo />
+      <CardWithForm />
+      <CardWithNotifications />
+      <DatePicker />
+      <AccordionDemo />
     </div>
   );
 }
